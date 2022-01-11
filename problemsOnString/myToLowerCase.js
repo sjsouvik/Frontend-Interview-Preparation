e95ebfe01937981bbe498333a8a25716bc048fa3
Souvik Jana <souvikjana2013@gmail.com>
2022-01-11 21:26:02 +0530

    feat: adde polyfill for toLowerCase() in JS

---
+++ problemsOnString/myToLowerCase.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * Write a polyfill for toLowerCase().
+ *
+ */
+
+String.prototype.myToLowerCase = function () {
+  const str = this;
+  let output = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const asciiOfChar = str.charCodeAt(i);
+
+    //ASCII value of "A" is 65, "a" is 97
+    if (asciiOfChar < 97) {
+      output += String.fromCharCode(asciiOfChar + 32);
+    } else {
+      output += str[i];
+    }
+  }
+
+  return output;
+};
+
+console.log("HellO".myToLowerCase());
