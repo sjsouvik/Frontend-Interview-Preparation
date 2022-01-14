0693abdde7d65f734237b0b1d4080f78b8b6d6b9
Souvik Jana <souvikjana2013@gmail.com>
2022-01-14 22:55:37 +0530

    feat: added few problems with solutions on currying

---
+++ closure/currying.js
@@ -0,0 +1,49 @@
+/**
+ *
+ * Write a function which would take input and give output as mentioned below:
+ *
+ * input: sum(3)(2)()
+ * output: 5
+ *
+ */
+
+function sum(a) {
+  return function (b) {
+    return function () {
+      return a + b;
+    };
+  };
+}
+
+const sumUsingArrowFunctions = (a) => {
+  return (b) => {
+    return () => {
+      return a + b;
+    };
+  };
+};
+
+const sumWithShortSyntax = first => second => _ => first + second; // if there's no argument passed to an arrow function, we can use '_' there
+
+console.log(sum(2)(3)());
+console.log(sumUsingArrowFunctions(2)(3)());
+console.log(sumWithShortSyntax(2)(3)());
+
+/**
+ *
+ * Advanced version of 1st problem:
+ *
+ * Write a function which would take input and give output as mentioned below:
+ *
+ * input: sum(1)(2)(3)...(n)()
+ * output: n(n + 1)/2
+ *
+ */
+
+function sumAdvanced(first) {
+  return function (second) {
+    return second ? sumAdvanced(first + second) : first;
+  };
+}
+
+console.log(sumAdvanced(1)(2)(3)());
