27cdcdd48d2d0c4073935b4f3d113e5f733ce1b5
Souvik Jana <souvikjana2013@gmail.com>
2022-01-15 16:53:19 +0530

    feat: add solution of a problem on currying, composition

---
+++ closure/composition.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Write a function compose() which can take any number of functions and return a function which will run those given funcitons in order when called with an argument. 
+ * 
+ */
+
+const increment = num => num + 1;
+const square = num => num * num;
+
+const compose = function (...functions){
+    return function (num){
+        return functions.reduce((accumulator, currentFunction)=>{
+            accumulator = currentFunction(accumulator);            
+            return accumulator;
+        }, num)
+    }
+}
+
+const returnedFunction = compose(increment, square, increment);
+console.log(returnedFunction(2));
