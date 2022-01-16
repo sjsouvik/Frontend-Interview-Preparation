cc6a344931e3026920689d8a072e2bfc9ed66b92
Souvik Jana <souvikjana2013@gmail.com>
2022-01-16 19:28:56 +0530

    feat: add a problem on JS object

---
+++ object/objectFunction.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * Write a function which would take input and give output as follows:
+ *
+ * plus(2).plus(3).value()
+ * output: 5
+ *
+ * plus(5).minus(3).value()
+ * 2
+ *
+ * plus(7).plus(3).minus(2).value()
+ * output: 8
+ *
+ */
+
+function minus(a) {
+  return {
+    value: function () {
+      return a;
+    },
+    plus: function (b) {
+      return plus(a + b);
+    },
+    minus: function (c) {
+      return minus(a - c);
+    },
+  };
+}
+
+function plus(a) {
+  return {
+    value: function () {
+      return a;
+    },
+    plus: function (b) {
+      return plus(a + b);
+    },
+    minus: function (c) {
+      return minus(a - c);
+    },
+  };
+}
+
+console.log(plus(5).minus(3).value());
+console.log(plus(7).plus(3).minus(2).value());
