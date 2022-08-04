ecd55fd141d619878191b01c6451a1beb3287fae
Souvik Jana <souvikjana2013@gmail.com>
2022-08-04 22:33:15 +0530

    Retry promise on rejection | Medium

---
+++ recursion/retry-promise-on-rejection.js
@@ -0,0 +1,38 @@
+/*
+
+For a web application, fetching API data is a common task.
+
+But the API calls might fail because of Network problems. Usually we could show a screen for Network Error and ask users to retry.
+
+One approach to handle this is auto retry when network error occurs.
+
+You are asked to create a fetchWithAutoRetry(fetcher, count), which automatically fetch again when error happens, until the maximum count is met.
+
+For the problem here, there is no need to detect network error, you can just retry on all promise rejections.
+
+***********************************************************************Solution***************************************************************/
+
+
+/**
+ * @param {() => Promise<any>} fetcher
+ * @param {number} maximumRetryCount
+ * @return {Promise<any>}
+ */
+function fetchWithAutoRetry(fetcher, maximumRetryCount) {  
+  return new Promise((resolve, reject) => {
+    let retryCount = 0;
+
+    function helper(){
+      fetcher().then(data => resolve(data)).catch(error => {        
+        if(retryCount++ < maximumRetryCount){
+          helper();
+        }else{
+          reject(error)
+        }
+      })
+    }
+
+    helper();    
+  })  
+}
+
