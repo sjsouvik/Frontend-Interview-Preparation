6da8bc99a7970ae9027e17f4a4306a4482f4aecc
Souvik Jana <souvikjana2013@gmail.com>
2023-07-15 23:51:42 +0530

    refactor: rename files

--- string/camelToSnakeCase.js
+++ string/camelToSnakeCase.js
@@ -1,13 +1,13 @@
 /*
 
-Given a input string convert the c++ variable to java variable
+Given a input string convert the C++ variable to java variable if it's a C++ variable and vice-versa
 
-c++ variables have underscore in them and java variable is written in camel case
+C++ variables have underscore in them and java variable is written in camel case
 
-input - this_is_a_variable (As it has underscore it is c++ we need to convert to Java)
+input - this_is_a_variable (As it has underscore it is C++ we need to convert to Java)
 output - thisIsAVariable
     
-input - meanVariableAverage(As it does not has underscore it is in Java so need to convert into c++)
+input - meanVariableAverage(As it does not has underscore it is in Java so need to convert into C++)
 output - mean_variable_average
 
 
