1698863c09510cdc403859453fd6c0e31e0579d2
Souvik Jana <souvikjana2013@gmail.com>
2023-10-21 20:11:51 +0530

    Update stringify.js

--- JSON/stringify.js
+++ JSON/stringify.js
@@ -17,46 +17,51 @@ JSON.stringify() support two more parameters which is not required here.
 
 *********************************************************************Solution**********************************************************/
 
-
 /**
  * @param {any} data
  * @return {string}
  */
- function stringify(data) {    
-    if([NaN, null, Infinity].includes(data)){
-      return 'null';
-    }
-  
-    if(typeof data === 'function' || data === undefined || typeof data === 'symbol'){
-      return undefined;
-    }
-    
-    if(typeof data === 'bigint'){
-      throw new Error("BigInt value can't be serialized in JSON");
-    }
-  
-    let output;
-  
-    if(Array.isArray(data)){        
-      output = `[${data.map(d => stringify(d))}]`;
-    }else if(data instanceof Date){
-      output = `"${data.toISOString()}"`;
-    }else if(typeof data === 'object'){
-      output = Object.entries(data).reduce((acc, [key, value])=>{
-        if(value === undefined){
-          return acc;
-        }
-  
-        acc.push(`"${key}":${stringify(value)}`);
-        return acc;
-      }, [])
-  
-      output = `{${output.join()}}`;
-    }else if(typeof data === 'string'){    
-      output = `"${data}"`;
-    }else{
-      output = String(data);
-    }
-  
-    return output;
+
+// asked in Wingify
+function stringify(data) {
+  if ([NaN, null, Infinity].includes(data)) {
+    return "null";
   }
+
+  if (
+    typeof data === "function" ||
+    data === undefined ||
+    typeof data === "symbol"
+  ) {
+    return undefined;
+  }
+
+  if (typeof data === "bigint") {
+    throw new Error("BigInt value can't be serialized in JSON");
+  }
+
+  let output;
+
+  if (Array.isArray(data)) {
+    output = `[${data.map((d) => stringify(d))}]`;
+  } else if (data instanceof Date) {
+    output = `"${data.toISOString()}"`;
+  } else if (typeof data === "object") {
+    output = Object.entries(data).reduce((acc, [key, value]) => {
+      if (value === undefined) {
+        return acc;
+      }
+
+      acc.push(`"${key}":${stringify(value)}`);
+      return acc;
+    }, []);
+
+    output = `{${output.join()}}`;
+  } else if (typeof data === "string") {
+    output = `"${data}"`;
+  } else {
+    output = String(data);
+  }
+
+  return output;
+}
