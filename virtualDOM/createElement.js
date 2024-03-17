6e683f9755ea4380b0a452d8915e3881fafe2b82
Souvik Jana <souvikjana2013@gmail.com>
2024-03-17 17:15:25 +0530

    basic implementation of `React.createElement()`

---
+++ virtualDOM/createElement.js
@@ -0,0 +1,91 @@
+/*
+
+Suppose you have solved above problem, now let's take a look at React.createElement():
+
+```
+React.createElement(
+  type,
+  [props],
+  [...children]
+)
+```
+
+1. First argument is type, it could be set to Custom Component, but here in this problem, it would 
+only be HTML tag name
+2. Second argument is props, here in this problem, it would only be the (common) camelCased HTML attributes
+3. the rest arguments are the children, which in React supports many data types, but in this problem, 
+it only has the element type of MyElement, or string for TextNode.
+
+You are asked to create your own createElement() and render(), so that following code could create the 
+exact HTMLElement in 113. Virtual DOM I.
+
+```
+const h = createElement
+
+render(h(
+  'div',
+  {},
+  h('h1', {}, ' this is '),
+  h(
+    'p',
+    { className: 'paragraph' },
+    ' a ',
+    h('button', {}, ' button '),
+    ' from ',
+    h('a', 
+      { href: 'https://bfe.dev' }, 
+      h('b', {}, 'BFE'),
+      '.dev')
+  )
+))
+```
+
+Notes:
+
+The goal of this problem is not to create the replica of React implementation, you can have your own 
+object representation format other than the one in 113. Virtual DOM I.
+
+Details about ref, key are ignored here, they will be put in other problems. Re-render is not covered here, 
+it will be in another problem as well.
+
+****************************************************Solution**************************************************/
+
+/**
+ * MyElement is the type your implementation supports
+ *
+ * type MyNode = MyElement | string
+ */
+
+/**
+ * @param { string } type - valid HTML tag name
+ * @param { object } [props] - properties.
+ * @param { ...MyNode} [children] - elements as rest arguments
+ * @return { MyElement }
+ */
+function createElement(type, props, ...children) {
+  const element = document.createElement(type);
+
+  for (let key of Object.keys(props)) {
+    const value = props[key];
+    key = key === "className" ? "class" : key;
+    element.setAttribute(key, value);
+  }
+
+  for (const child of children) {
+    if (typeof child === "string") {
+      element.append(document.createTextNode(child));
+    } else {
+      element.append(child);
+    }
+  }
+
+  return element;
+}
+
+/**
+ * @param { MyElement }
+ * @returns { HTMLElement }
+ */
+function render(myElement) {
+  return myElement;
+}
