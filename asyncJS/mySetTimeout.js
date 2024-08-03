9f05351623e9e0ddfb4a247ae00435cbc2045a90
Souvik Jana <souvikjana2013@gmail.com>
2024-08-03 22:15:05 +0530

    polyfill of `setTimeout()` | Medium

---
+++ asyncJS/mySetTimeout.js
@@ -0,0 +1,46 @@
+/*
+
+Write a polyfill of `setTimeout()`
+
+****************************************************Solution*************************************************/
+
+/* 
+
+Asked in Target
+
+https://www.youtube.com/watch?v=j1-fbe5F5uE&ab_channel=JsCafe
+
+requestIdleCallback() only present in browser or, window object, so, this code would only work in browser
+ - https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback 
+ 
+*/
+
+let timerId = 0;
+const timerMap = {};
+
+globalThis.mySetTimeout = function (callback, delay, ...args) {
+  const id = timerId++;
+  const startTime = Date.now();
+  timerMap[id] = true;
+
+  function triggerCallback() {
+    if (timerMap[id] === undefined) {
+      return;
+    }
+
+    if (Date.now() >= startTime + delay) {
+      callback.apply(this, args);
+    } else {
+      requestIdleCallback(triggerCallback); // without `requestIdleCallback()`, the call stack size would exceed
+    }
+  }
+
+  requestIdleCallback(triggerCallback);
+  return id;
+};
+
+window.myClearTimeout = function (id) {
+  delete timerMap[id];
+};
+
+globalThis.mySetTimeout((name) => console.log("hello", name), 100, "Souvik");
